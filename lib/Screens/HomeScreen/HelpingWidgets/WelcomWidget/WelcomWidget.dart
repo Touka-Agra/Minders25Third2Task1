81b7707fb5de09c53750eb36bb5266e81c7f3e4d
Touka-Agra <touka.agra@gmail.com>
2025-07-04 05:25:23 +0300

    WelcomeWidget

---
+++ lib/Screens/HomeScreen/HelpingWidgets/WelcomWidget/WelcomWidget.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class WelcomeWidget extends StatelessWidget {
+  const WelcomeWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Hi Hala,", style:TextStyle(fontSize: 25,fontWeight: FontWeight.w500)),
+        Text.rich(
+          TextSpan(
+            text: 'You have ', // default style
+            style: TextStyle(color: Colors.black54, fontSize: 14, fontWeight: FontWeight.w500),
+            children: [
+              TextSpan(
+                text: '4 pending test ',
+                style: TextStyle(color: Colors.red), // different color
+              ),
+              TextSpan(
+                text: 'this week',
+              ),
+            ],
+          ),
+        )
+
+        //Text()
+      ],
+    );
+  }
+}
